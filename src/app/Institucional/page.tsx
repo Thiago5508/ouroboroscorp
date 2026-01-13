9ba67b070e9e59e15edbf2b3c1acc9bc9e47bb8f
deadman5508 <deadman5508@gmail.com>
2026-01-12 21:25:27 -0300

    first commit

--- src/app/Institucional/page.tsx
+++ src/app/Institucional/page.tsx
@@ -1,28 +1,39 @@
 import Container from "@/components/container";
 import foto1 from '@/assets/foto1.png'
-import foto2 from '@/assets/foto2.jpg'
+import foto2 from '@/assets/foto2.png'
 import Image from "next/image";
+import { FiInstagram, FiLinkedin } from "react-icons/fi";
+import { FaBehance, FaGithub } from "react-icons/fa";
+import Social from "@/components/Social";
 
 export default function Institucional() {
  return (
     <Container>
-      <main className= "mx-auto text-black w-full h-max flex flex-col gap-8 row-start-2 items-center sm:items-start text-xl">
-        <div className="bg-white text-black rounded-md px-4 py-4 my-4 text-justify max-w-4xl items-center  mx-auto">
-            <h1 className="sm:text-2xl text-xl  my-4 items-center sm:ml-10 sm:w-2/4">Sobre a Oub</h1>
+      <main className= "mx-auto text-black w-full h-max text-xl flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <div className="bg-white text-black rounded-md px-4 py-4 my-4 text-justify max-w-xl items-center  mx-auto">
+            <h1 className="sm:text-2xl text-xl items-center my-4 ">Sobre a Oub</h1>
 
-            <div className="text-lg  items-center justify-center ">
+            <div className="text-lg  items-center justify-center gap-4 flex flex-col mb-4 ">
               <p>Desenvolvemos soluções em design gráfico e sistemas digitais, integrando design e tecnologia por meio de processos centrados no ser humano. </p>
               <p>Atuamos com o objetivo de ser referência no desenvolvimento de soluções digitais orientadas pela centralidade no ser humano, acessibilidade como princípio, usabilidade e clareza.</p>
             </div>
 
-            <div className="flex flex-col md:flex-row mt-6 ">
+            <h2 className="mt-8">Nossa equipe</h2>
+
+            <div className="flex flex-col md:flex-row mt-4 items-center justify-between">
               <div className="flex flex-col items-center  gap-6 mt-6">
-                <div className="flex sm:w-1/4">
-                  <Image src={foto1} alt="foto1" />
+                <div className="flex flex-row">
+                  <div className="flex flex-col gap-2 items-center justify-center">
+                    <FiLinkedin size={30}/>
+                    <FaBehance size={30}/>
+                    <FiInstagram size={30}/>
+                    <FaGithub size={30}/>
+                  </div>
+                  <Image src={foto1} alt="foto1" className="h-56 w-56"/>
                 </div>
                 
-                <div className="flex flex-col sm:w-2/4 gap-2" >
-                  <h2 className="text-xl font-semibold sm:w-2/4 mb-10">Jamisson Machado</h2>
+                <div className="flex flex-col  gap-2" >
+                  <h2 className="text-xl font-semibold mb-4">Jamisson Machado</h2>
                   <p>Designer de Interfaces</p>
                   <p> Desenvolvedor Front-End</p>
                 </div>
@@ -30,11 +41,16 @@ export default function Institucional() {
 
               <div className="flex  flex-col items-center">
                 <div className="flex flex-col items-center  gap-6 mt-6">
-                  <div className="flex sm:w-1/4">
-                    <Image src={foto2} alt="foto1" className="rounded-lg"/>
+                  <div className="flex flex-row">
+                    <div className="flex flex-col gap-2 items-center justify-center">
+                      <FiLinkedin size={30}/>
+                      <FiInstagram size={30}/>
+                      <FaGithub size={30}/>
+                    </div>
+                    <Image src={foto2} alt="foto1" className=" h-56 w-56 flex"/>
                   </div>
-                  <div className="flex flex-col sm:w-2/4 gap-4 " >
-                  <h2 className="text-xl font-semibold mb-10">Thiago Oliveira</h2>
+                  <div className="flex flex-col  gap-2" >
+                  <h2 className="text-xl font-semibold mb-4">Thiago Oliveira</h2>
                     <p> Desenvolvedor Full-Stack</p>
                     <p>Analista de Banco de Dados</p>
                     </div>
@@ -43,6 +59,7 @@ export default function Institucional() {
 
             </div>
         </div>
+        <Social/>
       </main>
     </Container>
   );
