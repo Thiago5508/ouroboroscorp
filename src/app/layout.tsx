d9a7abf13da407b397fe00bd842dc17be35b9650
deadman5508 <deadman5508@gmail.com>
2026-02-06 22:40:15 -0300

    Fix git

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import Footer from "@/components/Footer";
 import { Montserrat,} from 'next/font/google';
 import Temp from "@/components/Temp";
 import Whatsapp from "@/components/Whatsapp";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const montserrat = Montserrat({
   subsets: ['latin'],
